e2b2f6baab940ce0abff105c96376af79470f3ae
mayurjathan <mayurjathan999@gmail.com>
2023-02-19 15:05:33 +0530

    added binary exp

---
+++ Reverse Engineering/vault door -1/solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class VaultDoor1 {
+    public static void main(String args[]) {
+    char[] password = new char[32];
+               password[0]  = 'd';
+               password[29] = '3';
+               password[4] ='r';
+               password[2] ='5';
+               password[23] = 'r';
+               password[3] ='c';
+               password[17] = '4';
+               password[1] ='3';
+               password[7] ='b';
+               password[10] = '_';
+               password[5] ='4';
+               password[9] ='3';
+               password[11] = 't';
+               password[15] = 'c';
+               password[8] ='l';
+               password[12] = 'H';
+               password[20] = 'c';
+               password[14] = '_';
+               password[6] ='m';
+               password[24] = '5';
+               password[18] = 'r';
+               password[13] = '3';
+               password[19] = '4';
+               password[21] = 'T';
+               password[16] = 'H';
+               password[27] = 'f';
+               password[30] = 'b';
+               password[25] = '_';
+               password[22] = '3';
+               password[28] = '6';
+               password[26] = 'f';
+               password[31] = '0';
+               System.out.println(password);
+    }
+}
